b6b953a516a4260a747a0900c35dba2a12541e7b
Technocality <105610111+Technocality@users.noreply.github.com>
2022-07-31 23:05:08 -0500

    Create Python_help.py

---
+++ Python_help.py
@@ -0,0 +1,43 @@
+print("RUN THIS PROGRAM IN PYTHON 3!!!")
+print("I would recommend using Visual Studio Code!")
+print("Go to https://www.w3schools.com/python/ for python help!")
+print("")
+
+# using # comments out the line
+
+# assigning a variable
+x = "The variable x has now been assigned."
+print(x)
+print("")
+
+# input function
+input()
+# assigning a variable using user input
+y = input()
+print("")
+
+# print function
+print("hello world")
+# printing a variable
+print(y)
+# printing many things
+print("text " + y)
+print("")
+
+# exit() exits the program
+
+# if else statements
+print("coming soon")
+print("")
+
+# examples
+print("Was this helpful?")
+helpful = input()
+if helpful == "yes" or helpful == "Yes":
+    print("good")
+    exit()
+if helpful == "no" or helpful == "No":
+    print("I am sorry")
+    exit()
+else:
+    print("please type yes or no next time")
